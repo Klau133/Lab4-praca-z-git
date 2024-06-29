c2bb6c8250c164bbd0c64dac3ef32edc12e3de59
Klaudia <dsw52731@student.dsw.edu.pl>
2024-06-29 14:39:35 +0200

    Dodaj opcję --init

--- lab4.sh
+++ lab4.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+REPO_URL="https://github.com/Klau133/Lab4-praca-z-git.git"
+SCRIPT_NAME=$(basename "$0")
 
 case "$1" in
   --date|-d)
     date
     ;;
-  --logs|-1)
+  --logs|-l)
     num_files=100
     if [ -n "$2" ]; then
       num_files=$2
     fi
     for ((i=1; i<=num_files; i++)); do
       echo "log$i.txt" > "log$i.txt"
-      echo "Nazwa skryptu: lab4.sh" >> "log$i.txt"
+      echo "Nazwa skryptu: $SCRIPT_NAME" >> "log$i.txt"
       date >> "log$i.txt"
     done
+    ;;
+  --init)
+    git clone "$REPO_URL" cloned_repo
+    export PATH=$PATH:$(pwd)/cloned_repo
+    echo "Repozytorium sklonowane i zaktualizowano PATH."
     ;;
   --help|-h)
     echo "Użycie: lab4.sh [OPCJA]"
